c9d6b725d952f30fd20033f01b0feff736133f93
tomm8231 <tomm8231@stud.kea.dk>
2022-11-08 13:38:54 +0100

    First commit

---
+++ src/main/java/com/example/teknologiminiprojekt/TeknologiMiniprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknologiminiprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiMiniprojektApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeknologiMiniprojektApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/teknologiminiprojekt/TeknologiMiniprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknologiminiprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiMiniprojektApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
